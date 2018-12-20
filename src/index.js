a5ec468547b920cd7a31e3e2cd76f4b14d0ab079
AaronBoehle <boehle.aaron@gmail.com>
2018-12-20 09:50:58 -0600

    gitignoreupdate

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/*
-Create a directory named src. This is where your (untranspiled) javascript code will live.
-    Inside of the src directory, create a file named index.js. This is the entry point of your application.
-    Inside of index.js, write a function named sayHello using a arrow function, that logs hello to the console. Then call that function.
-*/
-"use strict";
-
-const sayHello = logHello =>console.log("Hello to the console!");
-sayHello();
